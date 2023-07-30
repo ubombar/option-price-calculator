10e7ba8660b29d9da18790d04d2c88c3d56f046f
ubombar <ufukbombar@gmail.com>
2023-07-30 23:27:31 +0200

    Add server main

---
+++ cmd/option-server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/sirupsen/logrus"
+	optionserver "github.com/ubombar/option-price-calculator/pkg/option-server"
+	pb "github.com/ubombar/option-price-calculator/pkg/option-service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	logrus.Infoln("Server Started")
+
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8080))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterOptionServiceServer(grpcServer, &optionserver.OptionServer{})
+	grpcServer.Serve(lis)
+
+}
