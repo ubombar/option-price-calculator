4bf8b49d738f6db961b5814c8b2a98c2992633e4
ubombar <ufukbombar@gmail.com>
2023-07-31 00:08:16 +0200

    Implement mock data

--- pkg/option-server/option-server.go
+++ pkg/option-server/option-server.go
@@ -3,6 +3,7 @@ package optionserver
 import (
 	"context"
 
+	"github.com/sirupsen/logrus"
 	pb "github.com/ubombar/option-price-calculator/pkg/option-service"
 )
 
@@ -11,5 +12,18 @@ type OptionServer struct {
 }
 
 func (s *OptionServer) OptionPrice(ctx context.Context, req *pb.OptionPriceRequest) (*pb.OptionPriceResponse, error) {
-	return nil, nil
+	logrus.Infoln("Server received a request")
+
+	var option_price float32 = 10.0
+
+	rep := &pb.OptionPriceResponse{
+		StockPrice:           req.StockPrice,
+		ExercisePrice:        req.ExercisePrice,
+		Volatility:           req.Volatility,
+		ExpirationTime:       req.ExpirationTime,
+		RiskFreeInterestRate: req.RiskFreeInterestRate,
+		OptionPrice:          option_price,
+	}
+
+	return rep, nil
 }
